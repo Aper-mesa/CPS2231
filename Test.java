f873fad394763489ddef80556cac9bee55e7c5cf
邹上豪 <zoush@kean.edu>
2023-02-28 12:32:01 +0800

    写完了

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ import eku.wku.sp23lab.ValidCardNumber;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(ValidCardNumber.isValidCard(4388576018402626L));
+        System.out.println(ValidCardNumber.isValidCard(4388576018410707L));
     }
 }
--- eku/wku/sp23lab/ValidCardNumber.java
+++ eku/wku/sp23lab/ValidCardNumber.java
@@ -6,12 +6,16 @@ public class ValidCardNumber {
         if (!(str.startsWith("4") || str.startsWith("5") || str.startsWith("37") || str.startsWith("6"))) return false;
         String strR = new StringBuilder(str).reverse().toString();
         int sum1 = 0;
-        for (int i = 0; i < strR.length(); i++) {
+        for (int i = 1; i < strR.length(); i++) {
             int digit = Integer.parseInt(strR.substring(i, i++ + 1)) * 2;
             if (digit > 9) digit = digit % 10 + digit / 10;
             sum1 += digit;
         }
-        System.out.println(sum1);
-        return true;
+        int sum2 = 0;
+        for (int i = 0; i < strR.length(); i++) {
+            int digit = Integer.parseInt(strR.substring(i, i++ + 1));
+            sum2 += digit;
+        }
+        return (sum1 + sum2) % 10 == 0;
     }
 }
