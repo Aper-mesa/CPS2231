fde1f8b68a645fa26601a760725f9fa80054a554
邹上豪 <zoush@kean.edu>
2023-03-03 20:23:29 +0800

    *

--- edu/wku/validkeanemail/ValidKeanEmail.java
+++ edu/wku/validkeanemail/ValidKeanEmail.java
@@ -2,8 +2,14 @@ package edu.wku.validkeanemail;
 
 public class ValidKeanEmail {
     public static int isValidKeanEmail(String address) {
+<<<<<<< Updated upstream
         if (address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)*@kean.edu")) return 2;
         if (address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)*@([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)*")) return 1;
+=======
+        if (address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)?@kean.edu") && address.length() >= 16 && address.length() <= 32)
+            return 2;
+        if (address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)?@([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)?")) return 1;
+>>>>>>> Stashed changes
         return -1;
     }
 }
