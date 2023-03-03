11e79c29e18e7980f333ea896d2d178d286f21c3
邹上豪 <zoush@kean.edu>
2023-03-03 20:27:31 +0800

    *

--- edu/wku/Test.java
+++ edu/wku/Test.java
@@ -7,6 +7,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(ValidKeanEmail.isValidKeanEmail("zoush@kean.edu"));
         System.out.println(ValidKeanEmail.isValidKeanEmail("zoush12345@kean.edu"));
-        System.out.println(ValidKeanEmail.isValidKeanEmail("1235425@kean.edu"));
+        System.out.println(ValidKeanEmail.isValidKeanEmail("1235425@kean.edu.cn"));
+        System.out.println(ValidKeanEmail.isValidKeanEmail("1235425@kean.edu.cn.wku"));
+        System.out.println(ValidKeanEmail.isValidKeanEmail("1.2.3.5.4.2.5@kean.edu.cn.wku"));
     }
 }
