5d69be3d15626cb06464d397a45c3f57d33bc067
邹上豪 <zoush@kean.edu>
2023-03-03 20:23:21 +0800

    *

--- edu/wku/validkeanemail/ValidKeanEmail.java
+++ edu/wku/validkeanemail/ValidKeanEmail.java
@@ -1,9 +1,9 @@
 package edu.wku.validkeanemail;
 
 public class ValidKeanEmail {
-    public static int isValidKeanEmail (String address) {
-        if(address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)?@kean.edu")) return 2;
-        if(address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)?@([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)?")) return 1;
+    public static int isValidKeanEmail(String address) {
+        if (address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)*@kean.edu")) return 2;
+        if (address.matches("([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)*@([a-zA-Z\\d]+)(.[a-zA-Z\\d]+)*")) return 1;
         return -1;
     }
 }
